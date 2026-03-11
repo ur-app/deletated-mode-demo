188a5d5b66aca71a5e164cd57796c9de0c40572e
Potter <potter.zhang@mantle.xyz>
2026-03-11 16:39:40 +0800

    Update Vite configuration to set base path for deployment and adjust asset paths in index.html. Remove obsolete JavaScript file from dist/assets.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+  base: "/deletated-mode-demo/",
   plugins: [react()],
   server: {
     port: 3000,
