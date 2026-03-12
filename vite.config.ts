59f19f9924d0316bf04b3c422666891a72b01218
Potter <potter.zhang@mantle.xyz>
2026-03-13 00:10:18 +0800

    Fixes base path typo and updates dev server

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: "/deletated-mode-demo/",
+  base: "/delegated-mode-demo/",
   plugins: [react()],
   server: {
     port: 3000,
+    open: "/delegated-mode-demo/",
     proxy: {
       "/v1": {
         target: "https://uropenapi.s7.gomantle.org",
